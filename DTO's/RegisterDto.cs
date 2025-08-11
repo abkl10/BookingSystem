f07ed26945b2e424cdcae1320c0df0b65f73857e
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-11 18:33:45 +0200

    added RegisterDto and LoginDto

---
+++ DTO's/RegisterDto.cs
@@ -0,0 +1,7 @@
+namespace BookingSystem.UserService.DTOs;
+
+public class RegisterDto
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- obj/Debug/net8.0/BookingSystem.UserService.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookingSystem.UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystem.UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4057fd0f30a40e9064bc545a956031b55afb7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystem.UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystem.UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
